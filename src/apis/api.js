43c7036570c360b5d7c8d83de2a95838ae1015a8
jaden <ljaden.9@gmail.com>
2022-11-28 18:48:43 -0500

    tempt fix - finnhub api key

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const TOKEN = process.env.REACT_APP_API_KEY;
-
+// const TOKEN = process.env.REACT_APP_API_KEY;
+const TOKEN = "cdp8iqaad3ifj5m9iv4gcdp8iqaad3ifj5m9iv50";
 export default axios.create({
   baseURL: "https://finnhub.io/api/v1/",
   params: {
